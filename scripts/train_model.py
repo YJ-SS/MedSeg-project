f41f4b6c4b23f3d73eb96b3b8b804b21920f1d2e
Yanjie-Z <905143096@qq.com>
2024-11-05 17:24:33 +0800

    train dual_MBConv_VAE 500 epoch

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,6 +2,6 @@ from seg_method.trainer import model_trainer
 if __name__ == '__main__':
     Trainer = model_trainer.SimpleTrainer(config_path='../train_configuration/test_dual_mbconv_vae.json')
     # Train model1
-    Trainer.train()
+    # Trainer.train()
     # Evaluate model1
     Trainer.evaluate()
